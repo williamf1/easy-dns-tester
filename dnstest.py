cc9581ef291d8a11d53495edb6ce7f3008907586
willfau <ken.faulkner.gh@gmail.com>
2019-07-06 16:48:51 +1000

    update-v2

--- dnstest.py
+++ dnstest.py
@@ -1,10 +1,24 @@
 from dns import resolver
+import time
+
+print("""not all websites will work because they
+have multiple dns servers to talk to so the program 
+will think that it is incorrect because it is talking
+to a different dns server every time you run the program
+and sometimes it will say it is correct when your dns server
+and 1.1.1.1 go to the same google dns server
+
+""")
+time.sleep(2)
+
 
 #dns user input
 
 website1=input("what website=")
-dnsserver=input("dns_server_ip=")
-
+print("     ")
+print("mind that this is your dns server it will be compaired with 1.1.1.1")
+dnsserver=input("your chosen dns server ip=")
+ 
 
 res = resolver.Resolver()
 
@@ -39,9 +53,6 @@ answers1 = res.query(website1)
 
 
 
-
-
-
 #output
 
 print("your chosen dns says that " +  website1 + "'s" +  " ip address is " +  answers[0].address)
@@ -54,14 +65,18 @@ print("     ")
 print("     ")
 
 
-# in two lines
+# output2
 
 print("1.1.1.1\t\t\tyour dns")
 print("     ")
 print(answers1[0].address + "\t\t" + answers[0].address)
 
 
+
+
+
+#same or not print
 if answers1[0].address == answers[0].address:
-    print("pass")
+    print("dns correct")
 else:
-    print("fail")
+    print("dns incorrect")

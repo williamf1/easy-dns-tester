a46947ea3568d0d21a26070b8745c4a75dbc9c54
willfau <ken.faulkner.gh@gmail.com>
2019-07-07 16:49:35 +1000

    update big
    before if a website had multiple dns servers it would be a fail but now it will say pass and everything is now aligned

--- dnstestmass.py
+++ dnstestmass.py
@@ -1,4 +1,37 @@
 from dns import resolver
+
+
+def compareresults(cloudflare,results):
+
+    cloudFlareIPList = []
+    resultsIPList = []
+
+    for res in cloudflare:
+        cloudFlareIPList.append( res.address)
+    
+    for res in results:
+        resultsIPList.append( res.address)
+    
+    cloudFlareIPList.sort()
+    resultsIPList.sort()
+    
+    #print(cloudFlareIPList)
+    #print(resultsIPList)
+
+
+    if len(cloudFlareIPList) != len(resultsIPList) :
+        return "_____fail1"
+
+    for x in range( len( cloudFlareIPList)) :
+        if cloudFlareIPList[x] != resultsIPList[x] :
+            return "_____fail2"
+
+
+    return "pass"
+
+
+
+
 #dns user input
 dnsserver=input("your chosen dns server ip=")
 res = resolver.Resolver()
@@ -7,6 +40,9 @@ res.nameservers = [dnsserver]
 res2 = resolver.Resolver()
 res2.nameservers = ["1.1.1.1"]
 a = open("testurls","r")
+
+print("website\t\t\t\t1.1.1.1           chosen dns server         result")
+
 while True:
     line = a.readline()
     if not line: 
@@ -16,14 +52,8 @@ while True:
     answeruser = res.query(website)
     answer2 = res2.query(website)    
     #print header
-    print("website\t\t\t1.1.1.1\t\t\t\tchosen dns server\t\tresult")
-    #same or not print 
-    if answer2[0].address == answeruser[0].address:
-        result="pass"
-    else:
-        result="fail"
-    #print answers
-    print(website + "\t\t"+answer2[0].address+"\t\t\t"+answeruser[0].address+"\t\t\t"+result)
+    result = compareresults(answer2,answeruser)
+    print('{0:<30} {1:<20} {2:<20} {3:>8}'.format(website, answer2[0].address, answeruser[0].address, result))
 
 
 
